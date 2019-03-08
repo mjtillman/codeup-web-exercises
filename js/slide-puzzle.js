7153086b92553ab108c3c9b51add90d8711ae4f9
mjtillman <mjtillman@gmail.com>
2019-03-07 19:45:21 -0600

    slide puzzle

--- js/slide-puzzle.js
+++ js/slide-puzzle.js
@@ -4,26 +4,43 @@ $(document).ready(function () {
     // let blank = '#place-' + Math.floor(Math.random() * 4);
 
     let blank = '';
-    let currentBlank = 'D';
+    let currentBlank = 'I';
 
     let place = {
         A: '#place-0',      // A: NW corner
-        B: '#place-1',      // B: NE corner
-        C: '#place-2',      // C: SW corner
-        D: '#place-3'       // D: SE corner
+        B: '#place-1',
+        C: '#place-2',      // C: NE corner
+        D: '#place-3',
+        E: '#place-4',
+        F: '#place-5',
+        G: '#place-6',      // G: SW corner
+        H: '#place-7',
+        I: '#place-8'       // H: SE corner
     };
 
     let state = [
         'class-0',
         'class-1',
         'class-2',
+        'class-3',
+        'class-4',
+        'class-5',
+        'class-6',
+        'class-7',
+        'class-8'
     ];
 
     function initialize() {
         $(place.A).toggleClass(state[0]);
         $(place.B).toggleClass(state[1]);
         $(place.C).toggleClass(state[2]);
-        $(place.D).toggleClass(blank);
+        $(place.D).toggleClass(state[3]);
+        $(place.E).toggleClass(state[4]);
+        $(place.F).toggleClass(state[5]);
+        $(place.G).toggleClass(state[6]);
+        $(place.H).toggleClass(state[7]);
+        $(place.I).toggleClass(blank);
+        console.log(currentBlank + ' is now blank.');
     }
 
     initialize();
@@ -41,23 +58,129 @@ $(document).ready(function () {
                 break;
             case 'ArrowDown':
                 downSwap();
-                break;
         }
     });
 
+    function upSwap() {
+        switch (currentBlank) {
+            case 'B':
+                $(place.B).removeClass(blank).addClass(state[4]);
+                $(place.E).removeClass(state[4]).addClass(blank);
+                state[4] = state[1];
+                state[1] = blank;
+                currentBlank = 'E';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'E':
+                $(place.E).removeClass(blank).addClass(state[7]);
+                $(place.H).removeClass(state[7]).addClass(blank);
+                state[7] = state[4];
+                state[7] = blank;
+                currentBlank = 'H';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'C':
+                $(place.C).removeClass(blank).addClass(state[5]);
+                $(place.F).removeClass(state[5]).addClass(blank);
+                state[2] = state[5];
+                state[5] = blank;
+                currentBlank = 'F';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'F':
+                $(place.F).removeClass(blank).addClass(state[8]);
+                $(place.I).removeClass(state[8]).addClass(blank);
+                state[5] = state[8];
+                state[8] = blank;
+                currentBlank = 'I';
+                console.log(currentBlank + ' is now blank.');
+                break;
+        }
+    }
+
     function downSwap() {
-        if (currentBlank === 'D') {                       //  swaps 2 and 0
-            $('#place-0').toggleClass(current2);
-            $('#place-2').toggleClass(current2);
-            current0 = current2;
-            current2 = blank;
-            currentBlank = 2;
-        } else if (currentBlank === 'C') {                //  swaps 1 and 3
-            $('#place-1').toggleClass(current3);
-            $('#place-3').toggleClass(current3);
-            current1 = current3;
-            current3 = blank;
-            currentBlank = 3;
+        switch (currentBlank) {
+            case 'G':
+                $(place.G).removeClass(blank).addClass(state[3]);
+                $(place.D).removeClass(state[3]).addClass(blank);
+                state[6] = state[3];
+                state[3] = blank;
+                currentBlank = 'D';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'E':
+                $(place.E).removeClass(blank).addClass(state[1]);
+                $(place.B).removeClass(state[1]).addClass(blank);
+                state[4] = state[1];
+                state[1] = blank;
+                currentBlank = 'B';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'H':
+                $(place.H).removeClass(blank).addClass(state[4]);
+                $(place.E).removeClass(state[4]).addClass(blank);
+                state[7] = state[4];
+                state[4] = blank;
+                currentBlank = 'E';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'F':
+                $(place.F).removeClass(blank).addClass(state[2]);
+                $(place.C).removeClass(state[2]).addClass(blank);
+                state[5] = state[2];
+                state[2] = blank;
+                currentBlank = 'C';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'I':
+                $(place.I).removeClass(blank).addClass(state[5]);
+                $(place.F).removeClass(state[5]).addClass(blank);
+                state[8] = state[5];
+                state[5] = blank;
+                currentBlank = 'F';
+                console.log(currentBlank + ' is now blank.');
+        }
+    }
+
+    function leftSwap() {
+        switch (currentBlank) {
+            case 'G':
+                $(place.G).removeClass(blank).addClass(state[7]);
+                $(place.H).removeClass(state[7]).addClass(blank);
+                state[6] = state[7];
+                state[7] = blank;
+                currentBlank = 'H';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'H':
+                $(place.H).removeClass(blank).addClass(state[8]);
+                $(place.I).removeClass(state[8]).addClass(blank);
+                state[7] = state[8];
+                state[8] = blank;
+                currentBlank = 'I';
+                console.log(currentBlank + ' is now blank.');
+                break;
+        }
+    }
+
+    function rightSwap() {
+        switch (currentBlank) {
+            case 'H':
+                $(place.H).removeClass(blank).addClass(state[6]);
+                $(place.G).removeClass(state[6]).addClass(blank);
+                state[7] = state[6];
+                state[6] = blank;
+                currentBlank = 'G';
+                console.log(currentBlank + ' is now blank.');
+                break;
+            case 'I':
+                $(place.I).removeClass(blank).addClass(state[7]);
+                $(place.H).removeClass(state[7]).addClass(blank);
+                state[8] = state[7];
+                state[7] = blank;
+                currentBlank = 'H';
+                console.log(currentBlank + ' is now blank.');
+                break;
         }
     }
 
